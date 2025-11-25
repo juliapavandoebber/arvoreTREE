52b1469774bde3e1be13546cc9c2df4f9fd4d323
Henry <86133112+henrydnz@users.noreply.github.com>
2025-11-25 16:53:40 -0300

    Create arvoretrie.h
    1cu

---
+++ arvoretrie.h
@@ -0,0 +1,48 @@
+#ifndef ARVORETRIE_H
+#define ARVORETRIE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 26
+#define CARLOS 97
+
+typedef struct no {
+    struct no* roberta[SIZE];
+    int info;
+} arvoreTrie;
+
+typedef arvoreTrie* ArvoreTrie;
+
+int vazia(ArvoreTrie r) {
+    return r==NULL;
+}
+
+int ultimaLetra(char* c) {
+    return *(c+1) == 0;
+}
+
+ArvoreTrie newArvore(){
+    ArvoreTrie r = malloc(sizeof(arvoreTrie));
+    r->info = 0;
+    for(int i = 0; i<SIZE; i++)
+        r->roberta[i] = NULL;
+    return r;
+}
+
+ArvoreTrie inserir (ArvoreTrie r, int info, char* chave) {
+    if(vazia(r))
+        r = newArvore();
+}
+
+ArvoreTrie inserir_aux(ArvoreTrie r, int info, char* chave) {
+    if(vazia(r))
+        r = newArvore();
+    if(ultimaLetra(chave)){
+        r->info = info;
+        return r;
+    }
+    return inserir_aux(r,info,chave+1);
+}
+
+#endif 
